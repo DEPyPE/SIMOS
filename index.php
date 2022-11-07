918c50733c08c592023ba61328d9a58691587a17
Wamor17 <wa_moreno@seg.guanajuato.gob.mx>
2022-11-07 14:15:22 -0600

    v0.3 - Index con imagen institucional de sistemas

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
 
+    <link href='https://fonts.googleapis.com/css?family=Baloo 2' rel='stylesheet'>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="Resource/Materialize/materialize.min.css"> 
@@ -19,8 +20,8 @@
     <?php  require_once("header.html") ?>
 
     <div class="row row-slider-and-login">
-        <div class="col l1"></div>
-        <div class="col l10">
+        <div class="col l2"></div>
+        <div class="col l8">
 
           <div class="cards-login-container">
             <div class="card card-slider">
@@ -31,28 +32,28 @@
                           <img class="slider-content" src="https://www.wrike.com/blog/content/uploads/2016/06/La-importancia-de-la-gestio-n-de-proyectos-preguntas-y-respuestas-con-Robert-Kelly.jpg">
                           <div class="caption center-align black-text">
                             <h3 class="">Administra</h3>
-                            <h5 class="light">Da de alta programas o proyectos sociales.</h5>
+                            <h5 class="light">Administra tus recomendaciones.</h5>
                           </div>
                         </li>
                         <li>
                           <img class="slider-content" src="https://conceptodefinicion.de/wp-content/uploads/2020/08/evaluacion.jpg">
                           <div class="caption left-align black-text">
-                            <h3>Evalúa</h3>
-                            <h5 class="light">Realiza la evaluación y agrega recomendaciones a los programas o proyectos. </h5>
+                            <h3>Posicionamiento</h3>
+                            <h5 class="light">Compartir tu opinión sobre la evaluación que recibiste.</h5>
                           </div>
                         </li>
                         <li>
                           <img class="slider-content" src="https://economipedia.com/wp-content/uploads/Seguimiento-de-un-proyecto.jpg">
                           <div class="caption right-align black-text">
                             <h3>Seguimiento</h3>
-                            <h5 class="light">Brinda un seguimiento a los proyectos evaluados.</h5>
+                            <h5 class="light">Realiza un seguimiento a los programas o proyectos evaluados.</h5>
                           </div>
                         </li>
                         <li>
                           <img class="slider-content" src="https://www.awerty.net/wp-content/uploads/2014/02/Compartir-documentos-1024x585.png">
                           <div class="caption center-align black-text">
-                            <h3>Comparte documentos</h3>
-                            <h5 class="light">Crea y comparte documentos sobre tu proyecto para que sean evaluados.</h5>
+                            <h3>Plan de mejora</h3>
+                            <h5 class="light">Crea y comparte plan de mejora de tu programa o proyecto.</h5>
                           </div>
                         </li>
                       </ul>
@@ -62,21 +63,20 @@
 
             <div class="card card-login">
               <div class="card-content">
-                  <h5 class="center-align title-login">Sistema de Monitoreo y Seguimiento a Recomendaciones (SIMOS)</h5><br>
 
                   <div class="input-field txtbox-input-user">
                       <i class="material-icons prefix">account_circle</i>
-                      <input id="txt_user" type="text" class="validate">
+                      <input id="txt_user" type="text" class="validate" placeholder="nombre@seg.guanajuato.gob.mx">
                       <label for="txt_user">Usuario</label>
                   </div>
 
                   <div class="input-field txtbox-input-password">
                       <i class="material-icons prefix">lock_outline</i>
-                      <input id="txt_password" type="password" class="validate">
+                      <input id="txt_password" type="password" class="validate" placeholder="Contraseña">
                       <label for="txt_password">Contraseña</label>
                   </div>
 
-                  <a class="btn waves-effect blue darken-1  btn-login   ">Iniciar sesión</a>
+                  <a class="btn waves-effect blue darken-1 btn-login-simos">Iniciar sesión</a>
 
                   
               </div>
@@ -84,7 +84,7 @@
           </div>
 
         </div>
-        <div class="col l1"></div>
+        <div class="col l2"></div>
     </div>
 
     <!-- INSTANCIAMOS EL FOOTER -->
